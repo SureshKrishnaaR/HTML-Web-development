25aec3e7c4f23987a67c073a225dad4e6b8a7eb4
Suresh Krishnaa R <progrsk24@gmail.com>
2020-08-12 11:07:57 +0530

    allchanged

---
+++ all trials/javascript trials/scripts/main.js
@@ -0,0 +1,8 @@
+/*let myimg=document.querySelector('img'); let
+btn=document.querySelector('button'); let myHeading =
+document.querySelector('h1'); myimg.onclick =function(){ let
+mysrc=myimg.getAttribute('src'); if(mysrc==='images/icecream2.jpg'){
+myimg.setAttribute('src','images/indian-dosa2.jpg');
+myimg.setAttribute('alt','indian-dosa'); } else{
+myimg.setAttribute('src','images/icecream2.jpg');
+myimg.setAttribute('alt','icecream'); } }*/
